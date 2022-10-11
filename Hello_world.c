6092301de328db854496fda24d2b1b44a9141017
cabeltes09 <115526954+cabeltes09@users.noreply.github.com>
2022-10-11 17:32:40 +0800

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+   //declaration of variables
+   int num1  = 1000;
+   char letter='A';
+   float num2 = 3.1416;
+
+   printf("value of num1 is %d/n",num1);
+   printf("value of letter is %c\n",letter);
+   printf("value of num2 is %f",num2);
+
+    return 0;
+}
